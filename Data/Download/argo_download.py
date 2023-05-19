99ee52c428c56c5ec5cf7d1e1f96a6947b658b76
kalkulator413 <50224596+kalkulator413@users.noreply.github.com>
2023-05-19 12:17:41 -0700

    Update argo_download.py

--- Data/Download/argo_download.py
+++ Data/Download/argo_download.py
@@ -1,12 +1,11 @@
-import os 
+import os
 from GeneralUtilities.Data.Filepath.instance import get_data_folder
-import subprocess 
+import subprocess
 
 argo_link = "https://www.seanoe.org/data/00311/42182/data/102270.tar.gz"
 argo_folder = os.path.join(get_data_folder(),'Raw/Argo/')
 
 def download():
-	subprocess.call(["wget", argo_link, "-P" ,argo_folder])
-	tarred_file = os.path.join(argo_folder, '102270.tar.gz')
-	subprocess.call(["tar", "-xvzf", tarred_file])
-	
+        subprocess.call(["wget", argo_link, "-P" ,argo_folder])
+        tarred_file = os.path.join(argo_folder, '102270.tar.gz')
+        subprocess.call(["tar", "-xvzf", tarred_file, '-C', argo_folder])
