08932341523e2d03719fc7ff7043efc0e46e2f43
Paul Chamberlain <pchamber@Pauls-MacBook-Pro.local>
2023-09-21 12:39:08 -0700

    bug fix

--- Data/Download/gom_mom6_download.py
+++ Data/Download/gom_mom6_download.py
@@ -3,8 +3,8 @@ import os
 from OptimalArray.Utilities.MOM6Mat import CovMOM6
 
 def download():
-for year_idx in range(2008,2019)
-		for month_idx in range(1,13):
-			url = 'https://zenodo.org/record/7591445/files/MOM6_GoM_bgc_phys_%04d_%02d.nc' % (year_idx,month_idx) 
-			filename = url.split("/")[-1]
-			execute_download(url,filename,CovMOM6.data_directory)
+	for year_idx in range(2008,2019)
+			for month_idx in range(1,13):
+				url = 'https://zenodo.org/record/7591445/files/MOM6_GoM_bgc_phys_%04d_%02d.nc' % (year_idx,month_idx) 
+				filename = url.split("/")[-1]
+				execute_download(url,filename,CovMOM6.data_directory)
