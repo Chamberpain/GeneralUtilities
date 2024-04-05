01f36c15e9e0fc82319f70ad44f1f89d3fa027f3
Paul Chamberlain <paulchamberlain@Pauls-Mac-Studio.local>
2024-04-05 11:16:22 -0700

    update to mom6 download for version2 downloads

--- Data/Download/gom_mom6_download.py
+++ Data/Download/gom_mom6_download.py
@@ -3,8 +3,8 @@ import os
 from OptimalArray.Utilities.MOM6Mat import CovMOM6GOM
 
 def download():
-	for year_idx in range(2008,2019):
+	for year_idx in range(2008,2015):
 			for month_idx in range(1,13):
-				url = 'https://zenodo.org/record/7591445/files/MOM6_GoM_bgc_phys_%04d_%02d.nc' % (year_idx,month_idx) 
+				url = 'https://zenodo.org/record/10223185/files/MOM6_GoM_bgc_phys_%04d_%02d.nc' % (year_idx,month_idx) 
 				filename = url.split("/")[-1]
 				execute_download(url,filename,CovMOM6GOM.data_directory)
